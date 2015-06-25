430a0ed0d6ca795dc7de2f3e19bb80256090c8c0
Tae <taewjin@hotmail.com>
2015-06-24 19:10:13 -0700

    added images to every search and linked the title to the website

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -7,13 +7,15 @@ class MainsController < ApplicationController
 	def create
 		item = params[:search_key]
 
-		url = "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=#{item}"
+		url = "http://www.amazon.com/s/url=search-alias%3Daps&field-keywords=#{item}"
 			dump = Array.new
 			price = Array.new
 			image = Array.new
+			link = Array.new
 			amazon = Nokogiri::HTML(open(url))
 			amazon_search_result = amazon.css('.s-result-item')
 			amazon_search_result.each do |item|
+				link.push(item.css('.a-row.a-spacing-none a').map {|a| a['href']})
 				if image.length < 5
 					image.push(item.css('a.a-link-normal.a-text-normal img').map { |img| img['src'] })
 				end
@@ -28,8 +30,7 @@ class MainsController < ApplicationController
 					end
 				end
 			end
-
-			@amazon = dump.zip(price, image)
+			@amazon = dump.zip(price, image, link)
 
 
 		url = "http://seattle.craigslist.org/search/sss?query=#{item}&sort=rel"
@@ -53,8 +54,10 @@ class MainsController < ApplicationController
 		url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=#{item}&Find.x=0&Find.y=0&Find=Find"
 		dump = Array.new
 		price = Array.new
+		image = Array.new
 		walmart = Nokogiri::HTML(open(url))
 		walmart.css(".js-tile.tile-landscape").each do |item|
+			image.push(item.css('.js-product-image img').map { |img| img['data-default-image'] })
 			dump.push(item.at_css('.js-product-title').text)
 			if item.at_css('.price-display').nil?
 				dump.pop()
@@ -63,14 +66,16 @@ class MainsController < ApplicationController
 		  	end
 		end
 		
-		@walmart = dump.zip(price)
+		@walmart = dump.zip(price, image)
 
 
 		url = "http://www.bestbuy.com/site/searchpage.jsp?st=#{item}&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&cp=1&nrp=15&sp=&qp=&list=n&iht=y&usc=All+Categories&ks=960&keys=keys"
 		dump = Array.new
 		price = Array.new
+		image = Array.new
 		bestbuy = Nokogiri::HTML(open(url))
 		bestbuy.css(".list-item").each do |item|
+			image.push(item.css('.thumb img').map { |img| img['src'] })
 			dump.push(item.at_css('.sku-title').text)
 			if item.at_css('.medium-item-price').nil?
 				dump.pop()
@@ -79,7 +84,7 @@ class MainsController < ApplicationController
 		  	end
 		end
 		
-		@bestbuy = dump.zip(price)
+		@bestbuy = dump.zip(price, image)
 			render '/mains/index'
 	end
 
